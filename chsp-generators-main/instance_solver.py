43ebafa7a2c333b4e1fb32dfeee18a8470ab497e
Gijs Koning <gijs-koning@live.nl>
2022-05-17 10:17:51 +0200

    Add solutions and problems numpy file

--- chsp-generators-main/instance_solver.py
+++ chsp-generators-main/instance_solver.py
@@ -1,55 +1,54 @@
 import time
 
 import numpy as np
-from minizinc import Instance, Model, Solver, Result
+from minizinc import Instance, Model, Solver
 
-def get_params_of_file(file):
-  params = {}
-  with open(file) as f:
-    for line in f:
-      split = line.split()
-      param, val = split[0], split[2][:-1]
-      params[param] = int(val)
-  return params
+def solve(linear_problems, solver_string,solver, i, do_print=False):
+    if do_print:
+      print(f"\nCurrently solving {str(i)}.dzn with solver {solver_string}")
+
+    model = Model("../neil_paper_data_code/wallace-hoist_cpaior20-submission.mzn")
+    params = linear_problems[i]
+    for param, v in params.items():
+      model[param] = v
+
+    instance = Instance(solver, model)
+    args = {'output-time': True, 'solver-statistics': True, 'time-limit': 300000}
+    start_time = time.time()
+    results = instance.solve(intermediate_solutions=False, **args)
+    duration = time.time() - start_time
+    if do_print:
+        print(results.statistics)
+        print(results.solution)
+        print(f'Duration: {duration:.3f}s')
+        if duration >= args['time-limit'] / 1000:
+          print(f"Example {i} timed out!")
+    if results.solution is None:
+      return None, None
+    return results, params
 
 def main():
     NUM_EXAMPLES = 5000
     solver_string = 'gecode'
 
     solver = Solver.lookup(solver_string)
-    for i in range(1, NUM_EXAMPLES + 1):
-        print(f"\nCurrently solving {str(i)}.dzn with solver {solver_string}")
+    solutions = []
 
-        model = Model("../neil_paper_data_code/wallace-hoist_cpaior20-submission.mzn")
-        param_file = f"instances/linear/{str(i)}.dzn"
-        model.add_file(param_file)
-        params = get_params_of_file(param_file)
-        input_dict = {}
-
-        instance = Instance(solver, model)
-        args = {'output-time': True, 'solver-statistics': True, 'time-limit': 300000}
-        start_time = time.time()
-        results = instance.solve(intermediate_solutions=False, **args)
-        duration = time.time() - start_time
-        print(results.statistics)
-        print(results.solution)
-        print(f'Duration: {duration:.3f}s')
-        if duration >= args['time-limit'] / 1000:
-            print(f"Example {i} timed out!")
-        if results.solution is None:
-            continue
-
+    param_file = "instances/linearproblems.npy"
+    linear_problems = np.load(param_file, allow_pickle=True)
+    for i in range(NUM_EXAMPLES + 1):
+      results, params = solve(linear_problems, solver_string, solver,  i)
+      if results is not None:
         output_dict = {k: getattr(results.solution, k) for k in results.solution.__dict__ if k[0] != '_'}
         output_dict['jobs'] = int(str(results).split('jobs ')[1])
-        np.save(f'instances/linear_solutions/{str(i)}.npy', [input_dict, output_dict])
-        # with open(f"instances/linear_solutions/{str(i)}.txt", "w") as fw:
-        #     fw.write(str(results.solution) +
-        #              f'\nDuration: {duration:.3f} seconds' +
-        #              f'\nSolver: {solver_string}')
+        solutions.append((params, output_dict))
+        if i %10 == 1:
+          print("Temp saving. Currently at index", i, 'of total', NUM_EXAMPLES+1)
+              # temp save
+          np.save('instances/linear_solutions.npy', solutions)
 
-        # print(results.solution)
-        # row = [i] + [(int(times.total_seconds() * 1000), amount) for times, amount in zip(results.statistics['time'], results.statistics['weekends'])]
+    np.save('instances/linear_solutions.npy', solutions)
 
-        # print(results)
+
 if __name__ == '__main__':
     main()
