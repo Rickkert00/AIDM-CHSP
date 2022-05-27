336d5029087f037b9371cd83b30985b22e6feef3
Gijs Koning <gijs-koning@live.nl>
2022-05-27 19:41:21 +0200

    Train with batches

--- imitation_learning/neural_network.py
+++ imitation_learning/neural_network.py
@@ -1,4 +1,5 @@
 import dgl.function as fn
+import numpy as np
 import torch
 # pylint: disable= no-member, arguments-differ, invalid-name
 import torch as th
@@ -196,35 +197,31 @@ class RemovalTimePredictor(nn.Module):
 
     def __init__(self):  # TODO find out out_features optimal sizes
         super().__init__()
-        self.egat1 = EGATConv(3, 1, 3, 1, 3, bias=True)
-        self.norm1_nodes = nn.BatchNorm1d(3)  # TODO find what normalization works best
-        self.norm1_edges = nn.BatchNorm1d(3)
         self.relu1 = nn.ReLU()
+        def gnn_block(inputs, outputs, heads):
+            egat = EGATConv(*inputs, *outputs, heads, bias=True)
+            output_shape = np.array(outputs)*heads
+            return egat, output_shape
+        edge_features = 3
+        heads = 3
+        self.egat1, self.output_shape1 = gnn_block(inputs=(3,1), outputs=(3,edge_features), heads=heads)
+        self.egat2, self.output_shape2 = gnn_block(inputs=self.output_shape1, outputs=(3,edge_features), heads=heads)
+        self.egat3, self.output_shape3 = gnn_block(inputs=self.output_shape2, outputs=(3,edge_features), heads=heads)
+        self.egat_final, _ = gnn_block(inputs=self.output_shape3, outputs=(1, 1), heads=1)
 
-        self.egat2_input_size_edges = 1 * 3  # times 3 because we use 3 attention heads
-        self.egat2_input_size_nodes = 3 * 3
-        self.egat2 = EGATConv(self.egat2_input_size_nodes, self.egat2_input_size_edges, 3, 1, 3, bias=True)
-        self.norm2_nodes = nn.BatchNorm1d(3)
-        self.norm2_edges = nn.BatchNorm1d(3)
-        self.egat3_input_size_edges = 1 * 3  # again times 3 as we use 3 heads in 2nd gat layer
-        self.egat3_input_size_nodes = 3 * 3
-        self.egat3 = EGATConv(self.egat3_input_size_nodes, self.egat3_input_size_edges, 1, 1, 1, bias=True)
+    def _forward_layer(self, graph, node_f, edge_f, gnn):
+        new_node_feats, new_edge_feats = gnn(graph, node_f, edge_f)  # output shape: N x Heads x out_feats
+        new_node_feats, new_edge_feats = self.relu1(new_node_feats), self.relu1(new_edge_feats)
+        # we flatten the multi heads into 1 dimension such that we can apply the GAT again:
+        nodes, edges = self.output_shape1
+        new_node_feats = new_node_feats.reshape(-1, nodes)
+        new_edge_feats = new_edge_feats.reshape(-1, edges)
+        return new_node_feats, new_edge_feats
 
     def forward(self, graph, node_f, edge_f):
-        new_node_feats, new_edge_feats = self.egat1(graph, node_f, edge_f)  # output shape: N x Heads x out_feats
-        new_node_feats = self.norm1_nodes(new_node_feats)
-        new_edge_feats = self.norm1_edges(new_edge_feats)
-        new_node_feats = self.relu1(new_node_feats)
-        new_edge_feats = self.relu1(new_edge_feats)
-        # we flatten the multi heads into 1 dimension such that we can apply the GAT again:
-        new_node_feats = new_node_feats.reshape(-1, self.egat2_input_size_nodes)
-        new_edge_feats = new_edge_feats.reshape(-1, self.egat2_input_size_edges)
-        new_node_feats, new_edge_feats = self.egat2(graph, new_node_feats, new_edge_feats)
-        new_node_feats = self.norm2_nodes(new_node_feats)
-        new_edge_feats = self.norm2_edges(new_edge_feats)
-        new_node_feats = self.relu1(new_node_feats)
-        new_edge_feats = self.relu1(new_edge_feats)
-        new_node_feats = new_node_feats.reshape(-1, self.egat3_input_size_nodes)
-        new_edge_feats = new_edge_feats.reshape(-1, self.egat3_input_size_edges)
-        new_node_feats, new_edge_feats = self.egat3(graph, new_node_feats, new_edge_feats)
+        new_node_feats, new_edge_feats = self._forward_layer(graph, node_f, edge_f, self.egat1)  # output shape: N x Heads x out_feats
+        new_node_feats, new_edge_feats = self._forward_layer(graph, new_node_feats, new_edge_feats, self.egat2)
+        new_node_feats, new_edge_feats = self._forward_layer(graph, new_node_feats, new_edge_feats, self.egat3)
+
+        new_node_feats, new_edge_feats = self.egat_final(graph, new_node_feats, new_edge_feats)
         return new_node_feats, new_edge_feats
--- imitation_learning/train.py
+++ imitation_learning/train.py
@@ -6,26 +6,32 @@ import time
 import dgl
 import numpy as np
 import torch
+from numpy import mean
 from torch import nn, optim
-from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ExponentialLR
+from torch_geometric.data import DataLoader
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 from imitation_learning.neural_network import RemovalTimePredictor
 
 INF = 9999
 
+
 def parse_args(_args=None):
     parser = argparse.ArgumentParser()
     # environment
     parser.add_argument('--num_train_steps', default=1000000, type=int)
-    parser.add_argument('--batch_size', default=1, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--hidden_dim', default=1024, type=int)
-    parser.add_argument('--learning_rate', default=1e-3, type=float)
+    parser.add_argument('--learning_rate', default=8e-4, type=float)
     # misc
     parser.add_argument('--seed', default=1, type=int)
 
     parser.add_argument('--log_interval', default=100, type=int)
     parser.add_argument('--work_dir', default='work_dir', type=str)
-    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--epochs', default=1000, type=int)
     args = parser.parse_args(_args)
     return args
 
@@ -44,78 +50,73 @@ def set_seed_everywhere(seed):
         torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
 
-
-def train(train_loader, model, optimizer, criterion, device):
-    """
-    Trains network for one epoch in batches.
-    Args:
-        train_loader: Data loader for training set.
-        model: Neural network model.
-        optimizer: Optimizer (e.g. SGD).
-        criterion: Loss function (e.g. cross-entropy loss).
-    """
-    model.train()  # model.train needs to be activated since the test method sets it to model.test. This is needed for batch normalization
-
+def _run(data_loader, model: RemovalTimePredictor, criterion, device, optimizer=None):
     avg_loss = 0
     correct = 0
     total = 0
-
     # Iterate through batches
-    for i, data in enumerate(train_loader):
+    for _, data in enumerate(data_loader):
         # Get the inputs; data is a list of [inputs, labels]
-        (graph_inputs, node_inputs, edge_inputs), labels = data
+        graph_inputs, labels = data
         # Move data to target device
-        node_inputs, edge_inputs, labels = node_inputs.to(device), edge_inputs.to(device), labels.to(device)
+        graph_inputs, labels = graph_inputs.to(device), labels.to(device)
 
-        # Zero the parameter gradients
         optimizer.zero_grad()
 
-        # Forward + backward + optimize
-        period, _ = model(graph_inputs, node_inputs, edge_inputs)
+        period, _ = model(graph_inputs, graph_inputs.ndata['x'], graph_inputs.edata['w'])
 
         # TODO find out why all values of period tensor are equivalent
-        loss = criterion(period[-1].squeeze(0), labels)  # calculate on the last tensor as that is the final tank
+        period_index = 0
+        output = torch.zeros(graph_inputs.batch_size)
+        for i in range(graph_inputs.batch_size):
+            g_nodes = graph_inputs._batch_num_nodes['_N'][i]
+            period_index += g_nodes
+            output[i] = period[period_index - 1]
+        loss = criterion(output, labels)  # calculate on the last tensor as that is the final tank
 
-        loss.backward()
-        optimizer.step()
+        if optimizer:
+            loss.backward()
+            optimizer.step()
 
         # Keep track of loss and accuracy
         avg_loss += loss
         total += labels.size(0)
-        # correct += torch.isclose(points_in_image.data, labels_in_image, atol=2).sum().item() // 8
+        correct += torch.isclose(output, labels, atol=5).sum().item()
 
-    return avg_loss.item() / len(train_loader)  # , 100 * correct / total
+    return avg_loss.item() / len(data_loader), 100 * correct / total
 
 
-def load_graph_data(device, training_size=0.8):
+def load_graph_data(training_size=0.8):
     solutions_and_input = np.load('../chsp-generators-main/instances/linear_solutions.npy', allow_pickle=True)
     input_data = solutions_and_input[:, 0]
     solutions = solutions_and_input[:, 1]
     num_nodes = [torch.from_numpy(np.array(input['Ninner'])).float() for input in input_data]
 
     # fully connected adj matrix with weights, including self connections
-    adjs = [torch.ones((int(num_tanks.item()+2), int(num_tanks.item()+2))) for num_tanks in
+    adjs = [torch.ones((int(num_tanks.item() + 2), int(num_tanks.item() + 2))) for num_tanks in
             num_nodes]  # obtain adjacency matrix, add 2 extra as we need start and end node
     # structure format: (tensor([0, 0, 0, 1, 1, 1, 2, 2, 2]), tensor([0, 1, 2, 0, 1, 2, 0, 1, 2]))
     # so structure[0][0] and structure[1][0] are connected etc.
     structure = list(zip([torch.nonzero(adj, as_tuple=True) for adj in
                           adjs]))  # edges list, 2 lists that indicate what nodes are connected
-    graphs = [dgl.graph((u_v[0][0], u_v[0][1]), device=device) for u_v in structure]
-
+    graphs = [dgl.graph((u_v[0][0], u_v[0][1])) for u_v in structure]
+    # g1.ndata['x'] = th.zeros(g1.num_nodes(), 3)
+    # g1.edata['w'] = th.ones(g1.num_edges(), 2)
     # offset input variable 'f' time as by 1 as f[0] is from initial stage to first tank, need to add that later
     node_feats = [[torch.from_numpy(np.array([0, INF, input['f'][idx]])) if idx == 0
-                   else torch.from_numpy(np.array([input['tmin'][idx-1], input['tmax'][idx-1], input['f'][idx]]))
-                   for idx in range(int(num_nodes[i].item())+1)]
-                   for i, input in enumerate(input_data)]  # need to have 3 features per node, and num_nodes nodes per problem. We add a start node already in this loop
+                   else torch.from_numpy(np.array([input['tmin'][idx - 1], input['tmax'][idx - 1], input['f'][idx]]))
+                   for idx in range(int(num_nodes[i].item()) + 1)]
+                  for i, input in
+                  enumerate(
+                      input_data)]  # need to have 3 features per node, and num_nodes nodes per problem. We add a start node already in this loop
     # now need to add final node
     for node_feat in node_feats:
-        node_feat.append(torch.from_numpy(np.array([0, INF, 0]))) # Add ending node
+        node_feat.append(torch.from_numpy(np.array([0, INF, 0])))  # Add ending node
 
     # convert node_feats to correct input shape and format
     corr_node_feats = []
     for tensors in node_feats:
-        corr_node_feats.append(torch.stack(tensors, dim=0))
-        corr_node_feats[-1] = corr_node_feats[-1].type(torch.float32)
+        corr_node_feats.append(torch.stack(tensors, dim=0).type(torch.float32))
     edge_feats = []
     # 'e' input param format: [[6  0  6 12], [12  6  0  6], [18 12  6  0], [0  6 12 18]] for 1 example instance
     # corr format would be: [0,6,12,18,0,6,0,6,12,0,12,6,0,6,0,0,0,0,0,0]
@@ -124,7 +125,7 @@ def load_graph_data(device, training_size=0.8):
         edge_array = input['e']
         # add start stage
         edge_feat.extend(np.append(edge_array[-1], 0))
-        for edge_num in range(int(num_nodes[idx].item())): # also include starting stage
+        for edge_num in range(int(num_nodes[idx].item())):  # also include starting stage
             # add in between stages
             edge_feat.extend(np.append(edge_array[edge_num], 0))
         # add final stage
@@ -133,8 +134,10 @@ def load_graph_data(device, training_size=0.8):
         # add edges to final stage
         float_edges = torch.Tensor(edge_feat).reshape(-1, 1)
         edge_feats.append(float_edges)
-
-    input_data = [(graphs[i], corr_node_feats[i], edge_feats[i]) for i in range(len(input_data))] # convert input into 1 tuple
+    for i in range(len(input_data)):
+        graphs[i].ndata['x'] = corr_node_feats[i]
+        graphs[i].edata['w'] = edge_feats[i]
+    input_data = [graphs[i]for i in range(len(input_data))]  # convert input into 1 tuple
     solved = [torch.from_numpy(np.array(d['objective'])).float().unsqueeze(dim=0) for d in
               solutions]  # TODO change this to 'r' if we want to train using removal times
     length = len(input_data)
@@ -152,36 +155,18 @@ def collate_fn(batches):
     :param batches: Tuple of input and output pairs that should be merged into a batch
     :return: returns the batched input samples
     """
-    graphs = []
-    node_f = None
-    edge_f = None
-    labels = None
-    for batch in batches:  # this will be batch size 1 for graph network
-        node_features = batch[0][1]
-        edge_features = batch[0][2]
-        label = batch[1]
-        graphs = batch[0][0]
-        if node_f is None:
-            node_f = node_features
-        if labels is None:
-            labels = label
-        if edge_f is None:
-            edge_f = edge_features
-            continue
-        else:
-            torch.stack((node_f, node_features), dim=0)
-            torch.stack((edge_f, edge_features), dim=0)
-            torch.stack((labels, label), dim=0)
-
-    return (graphs, node_f, edge_f), labels
+    graphs, labels = map(list, zip(*batches))
+    batched_graph = dgl.batch(graphs)
+    return batched_graph, torch.Tensor(labels)
 
 
 def main(_args=None):
+    debug = False
     print("args", _args)
     args = parse_args(_args)
     if args.seed == -1:
         args.__dict__["seed"] = np.random.randint(1, 1000000)
-    exp_id = str(int(np.random.random() * 100000))
+    exp_id = 'test'
     set_seed_everywhere(args.seed)
     # make directory
     ts = time.gmtime()
@@ -198,19 +183,59 @@ def main(_args=None):
         json.dump(vars(args), f, sort_keys=True, indent=4)
 
     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = torch.device('cpu')
-    folder = '../chsp-generators-main/instances/linear_solutions/'
-    train_set, test_set = load_graph_data(device)
-    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
-    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=True)
+    device = torch.device('cpu')  # Use cpu to debug faster
+    print("Using device:", device)
+    train_set, test_set = load_graph_data()
+    # train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
+    train_loader = dgl.dataloading.GraphDataLoader(train_set, batch_size=args.batch_size, collate_fn=collate_fn, shuffle=True, drop_last=True)
+    print('len training', len(test_set))
+
+    test_set = test_set[len(test_set)//args.batch_size*args.batch_size]
+    print('test_set', len(test_set))
+    # test_loader = dgl.dataloading.GraphDataLoader(test_set, batch_size=args.batch_size, collate_fn=collate_fn, drop_last=True)
+
+    if not debug:
+        writer = SummaryWriter()
 
     model = RemovalTimePredictor()
+    model.to(device)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), args.learning_rate)  # , weight_decay=args.weight_decay)
+    scheduler = ExponentialLR(optimizer, gamma=0.992)
+    train_time_avg = []
+    for epoch in range(args.epochs):
+        start_time = time.time()
+        model.train()  # model.train needs to be activated since the test method sets it to model.test. This is needed for batch normalization
+        train_loss, train_accuracy = _run(train_loader, model, criterion, device, optimizer)
+        train_time = time.time() - start_time
+        train_time_avg.append(train_time)
 
-    for i in range(args.epochs):
-        loss = train(train_loader, model, optimizer, criterion, device)
-        print(loss)
+        # print('train step seconds:', mean(train_time_avg).round(3))
+        test_loss, test_accuracy = None, None
+        # if epoch % 4 == 1:
+        #     model.eval()
+        #     with torch.no_grad():
+        #         test_loss, test_accuracy = _run(test_loader, model, criterion, device)
+
+        print("epoch", epoch, "train_loss", round(train_loss,1), "train_accuracy", round(train_accuracy,2))
+
+        if not debug:
+            loss_d = {
+                'Train_GNN': train_loss,
+            }
+            acc_d = {
+                'Train_GNN': train_accuracy,
+            }
+            if test_loss:
+                loss_d['Test_GNN'] = test_loss
+                acc_d['Test_GNN'] = test_accuracy
+            writer.add_scalars('Loss', loss_d, epoch)
+            writer.add_scalars('Accurary', acc_d, epoch)
+        scheduler.step()
+        print("lr:", round(scheduler.get_last_lr()[0],8))
+    if not debug:
+        writer.flush()
+        writer.close()
     # if args.model_dir is not None:
     #   pass
     # agent.load(args.model_dir, args.model_step)
